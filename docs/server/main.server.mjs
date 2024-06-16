bff1a51b39a44685cee0473ae3bc6ee83f69ad13
Vaibhav Dhole <vaibhav123dhole@gmail.com>
2024-06-16 09:58:35 +0530

    Minor css change

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3R6IPDYY.mjs";import"./chunk-SAQFZOOG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-EP4KKMJF.mjs";import"./chunk-SAQFZOOG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
